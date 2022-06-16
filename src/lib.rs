831e9c6aab79aa06c33ff9c2bda12d28f633166f
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-06-16 14:22:29 +0200

    test: length of all codes

--- src/lib.rs
+++ src/lib.rs
@@ -85,6 +85,7 @@ mod tests {
             assert_eq!(true, all_codes.contains(&random_code));
         }
         assert!(all_codes.iter().all(|code| code.is_unique()));
+        assert_eq!(all_codes.len(), 6*5*4*3);
     }
 
     #[test]
